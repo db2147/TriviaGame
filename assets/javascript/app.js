0b5c85f47a4969251fd06c5edf901c036b1d963d
David Bierma <31113045+db2147@users.noreply.github.com>
2017-10-17 16:56:46 -0500

    <javascript>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -67,11 +67,56 @@ questions[9] = {
 
 for (var i = 0; i < questions.length; i++) {
 	 $("#questionsDiv").append( "<h3>"+questions[i].question+"</h3>" );
-	 $("#questionsDiv").append('<label><input type="radio" name="question' + i + '" value="0">'+questions[i].option[0] + '</label></br>;
-	 $("#questionsDiv").append( "<li class='list-group-item list-group-item-action'>"+questions[i].option[1]+"</li>" );
-	 $("#questionsDiv").append( "<li class='list-group-item list-group-item-action'>"+questions[i].option[2]+"</li>" );
-	 $("#questionsDiv").append( "<li class='list-group-item <list-group-item-action></list-group-item-action'>"+questions[i].option[3]+"</li>" );
+	 $("#questionsDiv").append('<label><input type="radio" name="question' + i + '" value="0">'+questions[i].option[0] + '</label></br>');
+	 $("#questionsDiv").append('<label><input type="radio" name="question' + i + '" value="1">'+questions[i].option[1] + '</label></br>');
+	 $("#questionsDiv").append('<label><input type="radio" name="question' + i + '" value="2">'+questions[i].option[2] + '</label></br>');
+	 $("#questionsDiv").append('<label><input type="radio" name="question' + i + '" value="3">'+questions[i].option[3] + '</label></br>');
 	}
+
+	$('#checkAnswers').click(checkAnswers)
+	$('#startGame').click(startTimer)
+
+	function checkAnswers(){
+		var qCorrect = 0;
+		var qIncorrect = 0;
+		var qUnanswered = 0;
+		for (var i = 0; i < questions.length; i++) {
+			var userAnswer = $('input[name=question'+i+']:checked').val();
+
+			if ( userAnswer == questions[i].aAnswer) {
+				qCorrect++;
+			} else if ( typeof(userAnswer) == 'undefined') {
+				qUnanswered++;				
+			} else {
+				qIncorrect++; 
+			}
+	}
+		// console.log('Number Correct '+qCorrect);
+		// console.log('Number Incorrect '+qIncorrect);
+		// console.log('Number Unanswered '+qUnanswered);
+		$("#resultsDiv").html( "");
+		$("#resultsDiv").append( "<h2>Here are your results</h3>");
+		$("#resultsDiv").append( "<h3>Number Correct "+qCorrect+"</h3>");
+		$("#resultsDiv").append( "<h3>Number Incorrect "+qIncorrect+"</h3>");
+		$("#resultsDiv").append( "<h3>Number Unanswered "+qUnanswered+"</h3>");
+	}
+
+		function startTimer() {
+		$("#resultsDiv").html( "");
+		$("#timer").html(" ");
+		j = 30;
+	    countdownTimer = setInterval(function() {
+	        $("#timer").html(j + " seconds remaining");
+	        j = j - 1;
+	        if (j < 0) {
+	            clearInterval(countdownTimer);
+				// $("#questionsDiv").html("Time's up!!! Here are your results:");
+				checkAnswers();
+	        }
+	    }, 1000);
+	}
+
+
 
 
 
